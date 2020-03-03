67f0a1e41dc936c7de1ab2b7b00e0f65b4dbdb13
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-03-03 21:21:04 +0530

    Paginate: Show 10 records per page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.order("created_at desc").page(params[:page])
+    @posts = Post.order("created_at desc").page(params[:page]).per(10)
   end
 
   # GET /posts/1
