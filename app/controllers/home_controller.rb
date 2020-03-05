f01f576b53f069152e358c16885fa5d54eeb1223
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-03-05 11:00:51 +0530

    Styled trix editor

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @posts = Post.order("created_at desc")
     respond_to do |format|
-      format.html # show.html.erb
+      format.html # index.html.erb
       format.json { render json: @posts }
      end
   end
