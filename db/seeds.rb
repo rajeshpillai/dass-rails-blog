5545c5efb7523cd2cb5be2364bd1ec3da91087e7
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-03-05 19:04:04 +0530

    Seed data updates. Added tags to post

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,20 +8,30 @@
 
 Post.destroy_all
 Category.destroy_all
+Tagging.destroy_all
 Tag.destroy_all
+User.destroy_all
+
 Category.create(title: "Programming")
 Category.create(title: "Web Development")
 Category.create(title: "Ruby on Rails")
 Category.create(title: "React")
 
-Tag.create(name: "javascript")
-Tag.create(name: "react")
-Tag.create(name: "ruby")
-Tag.create(name: "node")
+javascript = Tag.create(name: "javascript")
+react = Tag.create(name: "react")
+ruby = Tag.create(name: "ruby")
+node = Tag.create(name: "node")
 
+admin_user = User.create(email: "rajesh@algorisys.com",
+        username:"admin",password:"123456",
+        password_confirmation:"123456")
 
 Category.all.each do |category|
     20.times do |i|
-        Post.create(title: "Post #{i}", body: "Post details", category: category)
+        # tag_offset =  rand(Tag.count)
+        # tag = Tag.offset(tag_offset).first
+        Post.create(title: "Post #{i}", 
+            body: "Post details", category: category,
+            tags: i % 2 == 0 ? [javascript, react] : [ruby, node])
     end
 end
